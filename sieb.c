7669e48ac61cfe8bf9d6a56c94b039a76c94baf6
Sherry <119943082+HonHonX@users.noreply.github.com>
2023-01-10 14:23:48 +0100

    Update sieb.c

--- sieb.c
+++ sieb.c
@@ -17,16 +17,18 @@ int main () {
 	printf("\n *   ´´´´´´   *                ©S.Buch * ");
 	printf("\n *************************************** \n\n");
 	
-	//Array füllen
-	int sieb[299];
-	for(int i=0; i<300; i++){
+	//Array füllen+
+	int size=99;
+	int sieb[size];
+	
+	for(int i=0; i<size+1; i++){
 		sieb[i]=i+1;
 		printf("%d | ", sieb[i]);
 	}
 	
-	for(int j=1;j<151;j++) {
+	for(int j=1;j<((size/2)+1);j++) {
 		if(sieb[j]!=0) {
-			for(int k=3;k<300;k++) {
+			for(int k=3;k<size+1;k++) {
 				if((sieb[k]%sieb[j]) == 0) {
 					sieb[k]=0;
 				}
@@ -34,7 +36,7 @@ int main () {
 		}
 	}
 	
-	ergebnisAusgeben(sieb, 299);
+	ergebnisAusgeben(sieb, size);
 	
 	//Ende
 	printf("\n\n");
