4438e0e7e363f5644b0af8c8b6ba4e6df29d425f
Sherry <119943082+HonHonX@users.noreply.github.com>
2023-01-10 20:47:38 +0100

    Update sieb.c

--- sieb.c
+++ sieb.c
@@ -50,7 +50,7 @@ int main () {
 int arrayFuellen(int array[], int size, int count) {
 	if(array[count]==1) {
 		printf("%d",array[count]);
-		array[count]=count+1;
+		//array[count]=count+1;
 	}
 	else if (count==size-1)
 		return 0;
